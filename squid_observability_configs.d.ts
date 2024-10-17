3dbc446ad56cf847d68b55b1f5fb6fe0fa9b0c88
Marcelo Morais <marcelo.fernandes@squidit.com.br>
2024-10-17 18:24:34 -0300

    adição de tipagem

--- squid_observability_configs.d.ts
+++ squid_observability_configs.d.ts
@@ -1,15 +1,102 @@
-declare module 'squid-observability-configs' {
-  export class SquidObservabilityConfigs {
-    #private;
-    constructor(projectId: any, credentials: any, environment: any, applicationName: any, version: any, applicationRepository?: any, applicationRevisionId?: any);
-    get projectId(): any;
-    get credentials(): any;
-    get serviceContext(): any;
-    get sourceReference(): any;
-    static SetGlobalConfig(observabilityConfigs: any): any;
-    static get projectId(): any;
-    static get credentials(): any;
-    static get serviceContext(): any;
-    static get sourceReference(): any;
-  }
+declare module "squid_observability_configs" {
+    export = ObservabilityConfigs;
+    /**
+     * @typedef {Object} ServiceAccountCredentials
+     * @property {string} [type]
+     * @property {string} [project_id]
+     * @property {string} [private_key_id]
+     * @property {string} [private_key]
+     * @property {string} [client_email]
+     * @property {string} [client_id]
+     * @property {string} [auth_uri]
+     * @property {string} [token_uri]
+     * @property {string} [auth_provider_x509_cert_url]
+     * @property {string} [client_x509_cert_url]
+     */
+    /**
+     * @typedef {Object} ServiceContext
+     * @property {'production' | 'staging' | 'local' | 'test' | string & {}} environment
+     * @property {string} service
+     * @property {string} version
+     * @property {string} applicationName
+     */
+    /**
+     * @typedef {Object} SourceReference
+     * @property {string} [repository]
+     * @property {string} [revisionId]
+     */
+    class ObservabilityConfigs {
+        /**
+         * @param {ObservabilityConfigs} observabilityConfigs
+         * @returns {ObservabilityConfigs}
+         */
+        static SetGlobalConfig(observabilityConfigs: ObservabilityConfigs): ObservabilityConfigs;
+        /** @returns {ObservabilityConfigs} */
+        static _GetGlobalConfig(): ObservabilityConfigs;
+        /** @returns {string} */
+        static get projectId(): string;
+        /** @returns {{credentials: ServiceAccountCredentials}} */
+        static get credentials(): {
+            credentials: ServiceAccountCredentials;
+        };
+        /** @returns {ServiceContext} */
+        static get serviceContext(): ServiceContext;
+        /** @returns {SourceReference} */
+        static get sourceReference(): SourceReference;
+        /**
+         * @param {string} projectId
+         * @param {string | object} credentials
+         * @param {'production' | 'staging' | 'local' | 'test' | string & {}} environment
+         * @param {string} applicationName
+         * @param {string} version
+         * @param {string} [applicationRepository]
+         * @param {string} [applicationRevisionId]
+         */
+        constructor(projectId: string, credentials: string | object, environment: "production" | "staging" | "local" | "test" | (string & {}), applicationName: string, version: string, applicationRepository?: string | undefined, applicationRevisionId?: string | undefined);
+        _projectId: string;
+        /** @type {ServiceContext} */
+        _serviceContext: ServiceContext;
+        /** @type {SourceReference} */
+        _sourceReference: SourceReference;
+        /** @type {{credentials: ServiceAccountCredentials}} */
+        _credentials: {
+            credentials: ServiceAccountCredentials;
+        };
+        /** @returns {string} */
+        get projectId(): string;
+        /** @returns {{credentials: ServiceAccountCredentials}} */
+        get credentials(): {
+            credentials: ServiceAccountCredentials;
+        };
+        /** @returns {ServiceContext} */
+        get serviceContext(): ServiceContext;
+        /** @returns {SourceReference} */
+        get sourceReference(): SourceReference;
+    }
+    namespace ObservabilityConfigs {
+        export { ServiceAccountCredentials, ServiceContext, SourceReference };
+    }
+    type ServiceAccountCredentials = {
+        type?: string | undefined;
+        project_id?: string | undefined;
+        private_key_id?: string | undefined;
+        private_key?: string | undefined;
+        client_email?: string | undefined;
+        client_id?: string | undefined;
+        auth_uri?: string | undefined;
+        token_uri?: string | undefined;
+        auth_provider_x509_cert_url?: string | undefined;
+        client_x509_cert_url?: string | undefined;
+    };
+    type ServiceContext = {
+        environment: "production" | "staging" | "local" | "test" | (string & {});
+        service: string;
+        version: string;
+        applicationName: string;
+    };
+    type SourceReference = {
+        repository?: string | undefined;
+        revisionId?: string | undefined;
+    };
 }
+//# sourceMappingURL=squid_observability_configs.d.ts.map
--- squid_observability_configs.js
+++ squid_observability_configs.js
@@ -1,109 +1,169 @@
-const { SquidError } = require('squid-error');
+// @ts-ignore
+const { SquidError } = require('squid-error')
 
 const squidObservabilityConfigsUniqueSymbol = Symbol.for('squidObservabilityConfigsSingleton');
 const globalSymbols = Object.getOwnPropertySymbols(global);
 
+/**
+ * @typedef {Object} ServiceAccountCredentials
+ * @property {string} [type]
+ * @property {string} [project_id]
+ * @property {string} [private_key_id]
+ * @property {string} [private_key]
+ * @property {string} [client_email]
+ * @property {string} [client_id]
+ * @property {string} [auth_uri]
+ * @property {string} [token_uri]
+ * @property {string} [auth_provider_x509_cert_url]
+ * @property {string} [client_x509_cert_url]
+ */
+
+/**
+ * @typedef {Object} ServiceContext
+ * @property {'production' | 'staging' | 'local' | 'test' | string & {}} environment
+ * @property {string} service
+ * @property {string} version
+ * @property {string} applicationName
+ */
+
+/**
+ * @typedef {Object} SourceReference
+ * @property {string} [repository]
+ * @property {string} [revisionId]
+ */
+
 class ObservabilityConfigs
 {
+  /**
+   * @param {string} projectId
+   * @param {string | object} credentials
+   * @param {'production' | 'staging' | 'local' | 'test' | string & {}} environment
+   * @param {string} applicationName
+   * @param {string} version
+   * @param {string} [applicationRepository]
+   * @param {string} [applicationRevisionId]
+   */
   constructor (projectId, credentials, environment, applicationName, version, applicationRepository, applicationRevisionId)
   {
     this._projectId = projectId;
 
-    this._serviceContext = {};
+    /** @type {ServiceContext} */
+    this._serviceContext = {
+      environment     : environment,
+      service         : `${applicationName} - ${environment}`,
+      version         : version,
+      applicationName : applicationName
+    };
 
-    this._serviceContext.environment     = environment;
-    this._serviceContext.service         = `${applicationName} - ${environment}`;
-    this._serviceContext.version         = version;
-    this._serviceContext.applicationName = applicationName;
+    /** @type {SourceReference} */
+    this._sourceReference = {
+      repository : applicationRepository,
+      revisionId : applicationRevisionId
+    };
 
-    this._sourceReference = {};
 
-    this._sourceReference.repository = applicationRepository;
-    this._sourceReference.revisionId = applicationRevisionId;
-
-    if (typeof credentials === 'string' || credentials instanceof String)
+    if (typeof credentials === 'string')
     {
       try
       {
-        this._credentials = {
-          credentials : JSON.parse(credentials)
-        };
+        /** @type {{credentials: ServiceAccountCredentials}} */
+        this._credentials = { credentials : JSON.parse(credentials) };
       }
       catch (error)
       {
-        this._credentials = {
-          keyFile     : credentials,
-          keyFilename : credentials
-        };
+        throw SquidError.Create({
+          message : 'Invalid credentials string provided for the Squid Observability library',
+          code    : 'SOC001',
+          detail  : { 
+            credentials,
+            originalErrorMessage: error instanceof Error ? error.message : 'unknown error',
+            originalErrorStack: error instanceof Error ? error.stack : 'unknown stack'
+          },
+          id      : 0
+        });
       }
     }
     else if (typeof credentials === 'object' && credentials !== null)
     {
-      this._credentials = {
-        credentials : credentials
-      };
+      this._credentials = { credentials }
     }
     else
     {
       throw SquidError.Create({
-        message : 'Invalid credentials provided for the Squid Observability library',
-        code    : 'SQUID_OBSERVABILITY_CONFIGS_INVALID_CREDENTIALS',
-        detail  : credentials,
+        message : 'Invalid credentials object provided for the Squid Observability library',
+        code    : 'SOC002',
+        detail  : { credentials },
         id      : 0
       });
     }
   };
 
+  /** @returns {string} */
   get projectId ()
   {
     return this._projectId;
   }
 
+
+  /** @returns {{credentials: ServiceAccountCredentials}} */
   get credentials ()
   {
     return this._credentials;
   }
 
+  /** @returns {ServiceContext} */
   get serviceContext ()
   {
     return this._serviceContext;
   }
 
+  /** @returns {SourceReference} */
   get sourceReference ()
   {
     return this._sourceReference;
   }
 
+  /**
+   * @param {ObservabilityConfigs} observabilityConfigs
+   * @returns {ObservabilityConfigs}
+   */
   static SetGlobalConfig (observabilityConfigs)
   {
-    const hasSymbol = (globalSymbols.indexOf(squidObservabilityConfigsUniqueSymbol) > -1);
 
-    if (!hasSymbol)
+    if (!globalSymbols.includes(squidObservabilityConfigsUniqueSymbol))
+      // @ts-ignore
       global[squidObservabilityConfigsUniqueSymbol] = observabilityConfigs;
 
+    // @ts-ignore
     return global[squidObservabilityConfigsUniqueSymbol];
   }
 
+  /** @returns {ObservabilityConfigs} */
   static _GetGlobalConfig ()
   {
+    // @ts-ignore
     return global[squidObservabilityConfigsUniqueSymbol];
   }
 
+  /** @returns {string} */
   static get projectId ()
   {
     return this._GetGlobalConfig().projectId;
   }
 
+  /** @returns {{credentials: ServiceAccountCredentials}} */
   static get credentials ()
   {
     return this._GetGlobalConfig().credentials;
   }
 
+  /** @returns {ServiceContext} */
   static get serviceContext ()
   {
     return this._GetGlobalConfig().serviceContext;
   }
 
+  /** @returns {SourceReference} */
   static get sourceReference ()
   {
     return this._GetGlobalConfig().sourceReference;
